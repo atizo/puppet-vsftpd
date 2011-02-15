01d043fc8bf694e3d7e2b8a60d90a2cbff7469a3
Simon Josi <simon.josi@atizo.com>
2011-02-15 14:52:44 +0100

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,28 @@
+#
+# vsftpd module
+#
+# Copyright 2011, Atizo AG
+# Simon Josi simon.josi+puppet(at)atizo.com
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
+#
+
+class vsftpd {
+  package{'vsftpd':
+    ensure => installed,
+  }
+  service{'vsftpd':
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    require => File['/etc/vsftpd/vsftpd.conf'],
+  }
+  file{'/etc/vsftpd/vsftpd.conf':
+    source => "puppet://$source/vsftpd.conf",
+    require => Package['vsftpd'],
+    owner => root, group => root, mode => 0600,
+  }
+}
